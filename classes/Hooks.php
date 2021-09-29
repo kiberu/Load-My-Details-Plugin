f037bbc249e53da7001c9342794c2b2423341520
Abu <kiberusharif@gmail.com>
2021-09-29 08:01:07 +0300

    Add load my details button on checkout page

--- classes/Hooks.php
+++ classes/Hooks.php
@@ -9,11 +9,15 @@ if ( ! class_exists( 'Hooks' ) ) {
 
         public function includes() {
             require_once( LMD_PATH . '/Activation.php' );
+            require_once( LMD_PATH . '/CheckoutButton.php' );
         }
 
         public function action_hooks() {
             $activation = new Activation();
+            $checkoutButton = new CheckoutButton();
+            
             add_action( 'init', array( $activation, 'check_if_woocommerce_installed' ) );
+            add_action( 'woocommerce_before_checkout_billing_form',  array( $checkoutButton, 'add_load_my_details_button' ) ); 
         }
     }    
 
